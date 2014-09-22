2821b03d75c18f51fa2c9a1bbe04ce2bd76e663c
apag101 <apag101@yahoo.com>
2014-09-22 08:32:57 -0400

    lesson 04.11

--- task_02.py
+++ task_02.py
@@ -7,14 +7,12 @@ Practice prompting inside a loop."""
 import data
 ACCESS = False
 COUNT = 0
-REPLIES = ["Not correct", "Umm Yes ?", "I do not remember"]
 TRACK = [3, 2, 1]
 
 while ACCESS == 0:
     msg = "What is your PASSWORD ({0} attempts left)?"
     P = raw_input(msg.format(TRACK[COUNT]))
     if P not in data.PASSWORD:
-        print REPLIES[COUNT]
         COUNT += 1
         if COUNT > 2:
             print "Access is denied, please try again later."
