7f33d1f4ade8277e5b06a275fb9d4fbccb211f26
apag101 <apag101@yahoo.com>
2014-09-22 00:04:33 -0400

    lesson 04.8

--- task_02.py
+++ task_02.py
@@ -10,7 +10,7 @@ COUNT = 0
 REPLIES = ["Not correct", "Umm Yes ?", "I do not remember"]
 TRACK = [3, 2, 1]
 
-while ACCESS != True:
+while ACCESS == 0:
     P = raw_input("What is your PASSWORD ({0} attempts left)?".format(TRACK[COUNT]))
     if P not in data.PASSWORD:
         print REPLIES[COUNT]
